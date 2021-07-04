d16aff0d51ecdc91ae0da654fe0991fb3d44ce83
Ashwin Km <kmuraliashwin@gmail.com>
2021-07-04 20:42:22 +0530

    Updated error message for tiny module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string! Please input only string data");
   return string.replace(/\s/g, "");
 };
